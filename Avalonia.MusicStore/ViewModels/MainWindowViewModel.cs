8225b42e58b630f1f8366f70e8c0ccb326e9f5ae
leonleerl <leon.runlongli@gmail.com>
2024-02-22 22:59:09 +0800

    Initial commit on Rider

---
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using ReactiveUI;
+
+namespace Avalonia.MusicStore.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public ICommand BuyMusicCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        BuyMusicCommand = ReactiveCommand.Create(() =>
+        {
+            // 当按钮被点击时，这里的代码将被执行。
+        });
+    }
+}
